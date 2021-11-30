4a50c574ad84fd30983d23ee2a8181ee32fa061c
nivamoli <github@nicovalencia.com.co>
2021-11-29 21:03:33 -0500

    agregando plugin de autopause con intersection observer

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,9 @@
 import MediaPlayer from "./MediaPlayer.js"; 
 import AutoPlay from "../plugins/AutoPlay.js"; // al usar scripts con type module debemos definir la extensión.
+import AutoPause from "../plugins/AutoPause.js"
 
 const video = document.querySelector("video")
-const player = new MediaPlayer({el: video, plugins: [new AutoPlay()]});
+const player = new MediaPlayer({el: video, plugins: [new AutoPlay(), new AutoPause()]});
 
 const button = document.getElementById("playButton")
 button.onclick = () => player.togglePlay();
