4230e56cd6f2a7a90bc0e1e18a082371870c5cb2
nivamoli <github@nicovalencia.com.co>
2021-11-29 21:19:05 -0500

    agregando funcionalidades de visualizacion con visibility change

--- assets/plugins/AutoPause.js
+++ assets/plugins/AutoPause.js
@@ -2,6 +2,7 @@ class AutoPause {
   constructor() {
     this.threshold = 0.25
     this.handleIntersection = this.handleIntersection.bind(this)
+    this.handleVisibilityChange = this.handleVisibilityChange.bind(this)
   }
   run(player) {
     this.player = player;
@@ -9,6 +10,8 @@ class AutoPause {
       threshold: this.threshold
     })
     observer.observe(this.player.media)
+
+    document.addEventListener("visibilitychange", this.handleVisibilityChange)
   }
 
   handleIntersection(entries) {
@@ -16,6 +19,15 @@ class AutoPause {
 
     const isVisible = entry.intersectionRatio >= this.threshold
 
+    if(isVisible) {
+      this.player.play();
+    } else {
+      this.player.pause();
+    }
+  }
+
+  handleVisibilityChange() {
+    const isVisible = document.visibilityState === "visible"
     if(isVisible) {
       this.player.play();
     } else {
